8b2dede8d15e636bdb316b9b7f02faa8701a3aa9
3222h <394954a+++@gmail.com>
2024-11-21 20:03:44 +0530

    Update android studio.sh

--- android studio.sh
+++ android studio.sh
@@ -7,16 +7,24 @@ mkdir devtools/android
 mkdir devtools/android/cmdline-tools
 
 cd devtools/JDK/
+
 wget https://builds.openlogic.com/downloadJDK/openlogic-openjdk/17.0.13+11/openlogic-openjdk-17.0.13+11-linux-x64.tar.gz
+
 cd devtools/android/cmdline-tools
+
 apt install unzip
+
 wget https://dl.google.com/android/repository/commandlinetools-linux-11076708_latest.zip
+
 unzip commandlinetools-linux-9477386_latest.zip
+
 mv cmdline-tools tools
 
-Set environment variables:
+###Set environment variables:
+
 nano ~/.bashrc
-add the code:
+
+###add the code:
 
 JAVA_HOME="/root/devtools/JDK/openlogic-openjdk-17.0.13+11-linux-x64"
 ANDROID_HOME="/root/devtools/android"
@@ -25,30 +33,36 @@ export ANDROID_HOME
 PATH="$JAVA_HOME/bin:$ANDROID_HOME/cmdline-tools/tools/bin:$ANDROID_HOME/platform-tools:$ANDROID_HOME/emulator:$PATH"
 
 source ~/.bashrc
-Check:
+
+###Check:
+
 java --version
+
 sdkmanager --version
 
-Install Android Platform and Build Tools:
+###Install Android Platform and Build Tools:
+
 sdkmanager --list
+
 sdkmanager "platform-tools" "platforms;android-33"
+
 sdkmanager "build-tools;33.0.2”
 
-Add android system image:
+###Add android system image:
 
 sdkmanager --list
 
 sdkmanager "system-images;android-33;google_apis_playstore;x86_64"
 
-*Creating the AVD:
+###Creating the AVD:
 
 avdmanager create avd --name “devAvd" --package "system-images;android-33;google_apis_playstore;x86_64"
 
-🔷 List avds:
+###List avds:
 
 emulator -list-avds
 
-🔷 Running the emulator:
+###Running the emulator:
 
 adb start-server
 
